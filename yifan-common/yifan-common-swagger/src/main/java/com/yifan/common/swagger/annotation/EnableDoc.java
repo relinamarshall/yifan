81c119c010ccac44a0db16c2972e9ba868ed91e9
刈凡 <10491794+yifanstudy@user.noreply.gitee.com>
2023-05-16 16:53:57 +0800

    change swagger oauth2

--- yifan-common/yifan-common-swagger/src/main/java/com/yifan/common/swagger/annotation/EnableDoc.java
+++ yifan-common/yifan-common-swagger/src/main/java/com/yifan/common/swagger/annotation/EnableDoc.java
@@ -17,6 +17,20 @@ import java.lang.annotation.Target;
  * EnableDoc
  * <p>
  * 开启 pig spring doc
+ * <p>
+ * http://192.168.139.233:9999/swagger-ui.html
+ * <p>
+ * http://192.168.139.233:9999/admin/v3/api-docs
+ * <p>
+ * username: admin
+ * <p>
+ * password:  relyxgMJ   # v3.4.2+ 以后输入密文
+ * <p>
+ * password:  123456     #  低版本输入 123456
+ * <p>
+ * client_id: test
+ * <p>
+ * client_secret:   test
  *
  * @author Wenzhou
  * @since 2023/5/5 19:04
